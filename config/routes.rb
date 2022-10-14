7f061b5c28eff37f909ea33511cd3c02db22f12a
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-10-14 14:49:35 +0000

    [Update]新規登録画面編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+    # 顧客用
+  # URL //sign_in ...
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
   scope module: :public do
     root 'homes#top'
     get 'homes/about', as: 'about'
@@ -78,17 +93,5 @@ Rails.application.routes.draw do
     resources :reviews, only: [:index, :destroy]
   end
 
-  # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :users, controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
 
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, controllers: {
-    sessions: "admin/sessions"
-  }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
