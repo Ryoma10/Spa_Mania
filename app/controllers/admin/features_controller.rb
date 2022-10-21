825dd225bc6c23d2c11fed96ad3bbba761787ab9
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-10-21 16:01:48 +0000

    [Update]カテゴリ追加

--- app/controllers/admin/features_controller.rb
+++ app/controllers/admin/features_controller.rb
@@ -1,11 +1,27 @@
 class Admin::FeaturesController < ApplicationController
   def index
     @feature = Feature.new
+    @bath_facilities = Feature.where(category: 'bath_facilities')
+    @sauna = Feature.where(category: 'sauna')
+    @other = Feature.where(category: 'other')
+    @building_facilities = Feature.where(category: 'building_facilities')
   end
 
   def create
+    feature = Feature.new(feature_params)
+    feature.save
+    redirect_to admin_features_path
   end
 
   def destroy
+    feature = Feature.find(params[:id])
+    feature.destroy
+    redirect_to admin_features_path
+  end
+
+  private
+  # ストロングパラメータ
+  def feature_params
+    params.require(:feature).permit(:name, :category)
   end
 end
