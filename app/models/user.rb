637a9b56effd0eef584fd63b295bd8b3a01855ef
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-12-22 15:06:14 +0000

    [Update]user_edit作成中

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,5 +30,5 @@ class User < ApplicationRecord
   # パスワードのバリデーション
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-
+  validates :introduction, length: {maximum: 200}
 end
